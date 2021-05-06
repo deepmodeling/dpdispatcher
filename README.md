66fc4192974c4065d0ebac82e35cdc1390f25cf0
Yuan Fengbo <yuanfengbo888@pku.edu.cn>
2021-05-06 13:43:42 +0800

    add use example

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 developers' discussion (in Chinese):
 https://www.yuque.com/docs/share/08ab09f3-f84d-4ed3-b777-9e0c791963b6?# 《dpdispatcher 讨论纪要》
 
+
+ussage example
+```python3
+
+
+machine = Machine.load_from_json_file(json_path='jsons/machine_local_shell.json')
+submission = Submission(work_base='parent_dir/', resources=machine.resources,  forward_common_files=['graph.pb'], backward_common_files=[])
+
+task1 = Task(command='cat example.txt', task_work_path='dir1/', forward_files=['example.txt'], backward_files=['out.txt'], outlog='out.txt')
+
+task2 = Task(command='cat example.txt', task_work_path='dir2/', forward_files=['example.txt'], backward_files=['out.txt'], outlog='out.txt')
+
+task3 = Task(command='cat example.txt', task_work_path='dir3/', forward_files=['example.txt'], backward_files=['out.txt'], outlog='out.txt')
+
+task4 = Task(command='cat example.txt', task_work_path='dir4/', forward_files=['example.txt'], backward_files=['out.txt'], outlog='out.txt')
+
+submission.register_task_list([task1, task2, task3, task4, ])
+submission.generate_jobs()
+submission.bind_batch(batch=machine.batch)
+submission.run_submission(clean=False)
+```
--- tests/test_shell_trival.py
+++ tests/test_shell_trival.py
@@ -5,6 +5,7 @@ from .context import Submission, Job, Task, Resources
 from .context import Shell
 from .context import LocalContext
 from .context import get_file_md5
+from .context import Machine
 
 import unittest
 
@@ -14,23 +15,25 @@ import unittest
 class TestShellTrival(unittest.TestCase):
     def setUp(self):
         self.maxDiff = None
-        self.local_context_dict = {
-            'local_root': './test_shell_trival_dir',
-            'remote_root': './tmp_shell_trival_dir'
-        }
+        # self.local_context_dict = {
+        #     'local_root': './test_shell_trival_dir',
+        #     'remote_root': './tmp_shell_trival_dir'
+        # }
     
     def test_shell_trival(self):
-        local_context = LocalContext.from_jdata(self.local_context_dict)
-        shell = Shell(local_context)
-        resources = Resources(number_node=1, cpu_per_node=4, gpu_per_node=0, queue_name="V100_8_32", group_size=2) 
-        submission = Submission(work_base='parent_dir/', resources=resources,  forward_common_files=['graph.pb'], backward_common_files=[])
+        # local_context = LocalContext.from_jdata(self.local_context_dict)
+        # shell = Shell(local_context)
+        machine = Machine.load_from_json_file(json_path='jsons/machine_local_shell.json')
+        # resources = machine.resources
+        # resources = Resources(number_node=1, cpu_per_node=4, gpu_per_node=0, queue_name="CPU", group_size=2) 
+        submission = Submission(work_base='parent_dir/', resources=machine.resources,  forward_common_files=['graph.pb'], backward_common_files=[])
         task1 = Task(command='cat example.txt', task_work_path='dir1/', forward_files=['example.txt'], backward_files=['out.txt'], outlog='out.txt')
         task2 = Task(command='cat example.txt', task_work_path='dir2/', forward_files=['example.txt'], backward_files=['out.txt'], outlog='out.txt')
         task3 = Task(command='cat example.txt', task_work_path='dir3/', forward_files=['example.txt'], backward_files=['out.txt'], outlog='out.txt')
         task4 = Task(command='cat example.txt', task_work_path='dir4/', forward_files=['example.txt'], backward_files=['out.txt'], outlog='out.txt')
         submission.register_task_list([task1, task2, task3, task4, ])
         submission.generate_jobs()
-        submission.bind_batch(batch=shell)
+        submission.bind_batch(batch=machine.batch)
         submission.run_submission(clean=False)
 
         for dir in ['dir1', 'dir2', 'dir3', 'dir4']:
