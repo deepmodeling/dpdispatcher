ad010cd6ac3c49c177dff9663aae7141639f36b5
Cloudac7 <812556867@qq.com>
2021-06-15 21:32:57 +0800

    rewrite class LSF; bump to latest dpdispatcher version

--- tests/devel_test_lsf.py
+++ tests/devel_test_lsf.py
@@ -1,80 +1,49 @@
 import os
 import sys
+import json
+from dpdispatcher.submission import Submission, Job, Task, Resources
+from dpdispatcher.machine import Machine
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from dpdispatcher.ssh_context import SSHSession, SSHContext
-from dpdispatcher.submission import Submission, Task, Resources
-from dpdispatcher.lsf import LSF
-
-# edit ssh parameters
-ssh_session = SSHSession(
-    hostname='127.0.0.1',
-    port=22,
-    remote_root='/home/dp/dpdispatcher/tests',
-    username='debug'
-)
-# local_root is the relevant path that containing tasks to be submit
-ssh_context = SSHContext(local_root='test_lsf_dir', ssh_session=ssh_session)
-lsf = LSF(context=ssh_context)
-
-prepend_text = '''
-module load cuda/9.2
-module load gcc/4.9.4
-module load deepmd/1.0
-source /home/dp/scripts/avail_gpu.sh
-'''
-
-lsf_bsub_dict = {
-    'R': "'select[hname != g005]'"
-}
-resources = Resources(
-    number_node=1,
-    cpu_per_node=4,
-    gpu_per_node=0,
-    queue_name="gpu",
-    walltime="24:00:00",
-    prepend_text=prepend_text,
-    append_text="",
-    gpu_usage=False,
-    gpu_new_syntax=False,
-    extra_specification=lsf_bsub_dict,
-    group_size=1
-)
-
 # task_need_resources has no effect
+with open("jsons/machine_lsf.json", 'r') as f:
+    mdata = json.load(f)
+
+machine = Machine.load_from_dict(mdata['machine'])
+resources = Resources.load_from_dict(mdata['resources'])
+
 submission = Submission(
-    work_base='0_md',  # the dir containing all of task_work_path
-    resources=resources,  # resources above
-    forward_common_files=['graph.pb'],  # file to be upload
-    backward_common_files=['*.json']  # file to be downloaded
+    work_base='0_md/',
+    machine=machine,
+    resources=resources,
+    forward_common_files=['graph.pb'],
+    backward_common_files=[]
 )
+
 task1 = Task(
-    command='lmp_mpi_20201029 -i input.lammps',
-    task_work_path='bct-1',
+    command='lmp -i input.lammps',
+    task_work_path='bct-1/',
     forward_files=['conf.lmp', 'input.lammps'],
-    backward_files=['log.lammps'],
+    backward_files=['log.lammps']
 )
 task2 = Task(
-    command='lmp_mpi_20201029 -i input.lammps',
-    task_work_path='bct-2',
+    command='lmp -i input.lammps',
+    task_work_path='bct-2/',
     forward_files=['conf.lmp', 'input.lammps'],
-    backward_files=['log.lammps'],
+    backward_files=['log.lammps']
 )
 task3 = Task(
-    command='lmp_mpi_20201029 -i input.lammps',
-    task_work_path='bct-3',
+    command='lmp -i input.lammps',
+    task_work_path='bct-3/',
     forward_files=['conf.lmp', 'input.lammps'],
-    backward_files=['log.lammps'],
+    backward_files=['log.lammps']
 )
 task4 = Task(
-    command='lmp_mpi_20201029 -i input.lammps',
-    task_work_path='bct-4',
+    command='lmp -i input.lammps',
+    task_work_path='bct-4/',
     forward_files=['conf.lmp', 'input.lammps'],
-    backward_files=['log.lammps'],
+    backward_files=['log.lammps']
 )
 submission.register_task_list([task1, task2, task3, task4, ])
-submission.generate_jobs()
-submission.bind_batch(batch=lsf)
-
-submission.run_submission()
+submission.run_submission(clean=True)
