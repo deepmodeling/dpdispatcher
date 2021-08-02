1d46b8cf06767246d3c1f331f9025138452c800e
Yuan Fengbo <yuanfengbo888@pku.edu.cn>
2021-08-02 16:01:44 +0800

    update ssh context unittest

--- tests/test_ssh_context.py
+++ tests/test_ssh_context.py
@@ -1,15 +1,27 @@
 import os,sys,json,glob,shutil,uuid,getpass
 import unittest
 from pathlib import Path
+from paramiko.ssh_exception import NoValidConnectionsError
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 __package__ = 'tests'
 from .context import SSHContext, SSHSession
-from .context import setUpModule
 
 class TestSSHContext(unittest.TestCase):
     def setUp(self):
         self.tmp_local_root = 'test_context_dir/'
         self.tmp_remote_root = 'tmp_ssh_context_dir/'
+
+        self.username = getpass.getuser
+        try:
+            self.ssh_session = SSHSession(
+                hostname='localhost',
+                username=self.username)
+        except NoValidConnectionsError:
+            self.skipTest("skip ssh tests due to ssh connection errors")
+
+    def test_ssh_session(self):
+        pass
+        # with self.
 
 
