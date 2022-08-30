6b4d829fb636d27ffc227d2d5f75288f2462a6aa
HuangJiameng <105633685+HuangJiameng@users.noreply.github.com>
2022-08-30 09:54:36 +0800

    create class TestSSHContextNoCompress

--- tests/test_ssh_context.py
+++ tests/test_ssh_context.py
@@ -108,12 +108,58 @@ class TestSSHContext(unittest.TestCase):
     def test_download(self):
         self.context.download(self.__class__.submission)
 
-    def test_tar_compress(self):
-        self.context.remote_profile['tar_compress'] = False
+class TestSSHContextNoCompress(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.server = mock_server()
+        host, port, username, workdir, key_filename = next(cls.server)
+        mdata = {
+            "batch_type": "Shell",
+            "context_type": "SSHContext",
+            "local_root": "./test_context_dir",
+            "remote_root": workdir,
+            "remote_profile": {
+                "hostname": host,
+                "port": port,
+                "username": username,
+                "key_filename": key_filename,
+                "tar_compress": False,
+            },
+        }
+        try:
+            cls.machine = Machine.load_from_dict(mdata)
+        except (SSHException, socket.timeout):
+            raise unittest.SkipTest("SSHException ssh cannot connect")
+        cls.submission = SampleClass.get_sample_submission()
+        cls.submission.bind_machine(cls.machine)
+        cls.submission_hash = cls.submission.submission_hash
+        file_list = ['bct-1/log.lammps', 'bct-2/log.lammps', 'bct-3/log.lammps', 'bct-4/log.lammps']
+        for file in file_list:
+            cls.machine.context.sftp.mkdir(os.path.join(cls.machine.context.remote_root, os.path.dirname(file)))
+            cls.machine.context.write_file(file, '# mock log')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.machine.context.clean()
+        # close the server
+        cls.machine.context.close()
+        next(cls.server)
+    
+    def setUp(self):
+        self.context = self.__class__.machine.context
+
+    def test_ssh_session(self):
+        self.assertIsInstance(
+            self.__class__.machine.context.ssh_session, SSHSession
+        )
+
+    def test_upload(self):
         self.context.upload(self.__class__.submission)
         check_file_list = ['graph.pb', 'bct-1/conf.lmp', 'bct-4/input.lammps']
         for file in check_file_list:
             self.assertTrue(self.context.check_file_exists(os.path.join(self.context.remote_root, file)))
+
+    def test_download(self):
         self.context.download(self.__class__.submission)
         
 
