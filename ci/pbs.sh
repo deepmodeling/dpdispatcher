16f69eb72754c2aae9e9d710ac83f5ccc3233c4b
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-09-12 17:15:52 -0400

    fix typo

--- ci/pbs.sh
+++ ci/pbs.sh
@@ -8,6 +8,6 @@ cd -
 docker exec pbs_master /bin/bash -c "chmod -R 777 /shared_space"
 docker exec pbs_master /bin/bash -c "chown -R pbsuser:pbsuser /home/pbsuser"
 
-docker exec pbs_master /bin/bash -c "cd /sdpdipatcher && pip install .[test] coverage codecov && chown -R pbsuser ."
-docker exec -u pbsuser pbs_master /bin/bash -c "cd /sdpdipatcher && coverage run --source=./dpdispatcher -m unittest -v && coverage report"
-docker exec -u pbsuser --env-file <(env | grep GITHUB) pbs_master /bin/bash -c "cd /sdpdipatcher && codecov"
+docker exec pbs_master /bin/bash -c "cd /dpdipatcher && pip install .[test] coverage codecov && chown -R pbsuser ."
+docker exec -u pbsuser pbs_master /bin/bash -c "cd /dpdipatcher && coverage run --source=./dpdispatcher -m unittest -v && coverage report"
+docker exec -u pbsuser --env-file <(env | grep GITHUB) pbs_master /bin/bash -c "cd /dpdipatcher && codecov"
