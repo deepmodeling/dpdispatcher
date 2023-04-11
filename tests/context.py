4fb8ccfb1d8d54d5221c8da98ad55c39874f6a7f
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-04-11 06:47:03 -0400

    ignore F401 in tests
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- tests/context.py
+++ tests/context.py
@@ -6,16 +6,37 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
+import dpdispatcher  # noqa: F401
+from dpdispatcher.base_context import BaseContext  # noqa: F401
+from dpdispatcher.distributed_shell import DistributedShell  # noqa: F401
+from dpdispatcher.dp_cloud_server import Lebesgue  # noqa: F401
+from dpdispatcher.hdfs_cli import HDFS  # noqa: F401
+from dpdispatcher.hdfs_context import HDFSContext  # noqa: F401
+
 # from dpgen.dispatcher.Dispatcher import FinRecord
+from dpdispatcher.JobStatus import JobStatus  # noqa: F401
 
 # from dpdispatcher.local_context import local_context
+from dpdispatcher.lazy_local_context import LazyLocalContext  # noqa: F401
 
 # from dpdispatcher.local_context import LocalSession
+from dpdispatcher.local_context import LocalContext, _identical_files  # noqa: F401
+from dpdispatcher.lsf import LSF  # noqa: F401
+from dpdispatcher.pbs import PBS  # noqa: F401
+from dpdispatcher.shell import Shell  # noqa: F401
+from dpdispatcher.slurm import Slurm  # noqa: F401
+from dpdispatcher.ssh_context import SSHContext, SSHSession  # noqa: F401
 
 try:
-    pass
+    from dpdispatcher.dp_cloud_server import DpCloudServer  # noqa: F401
+    from dpdispatcher.dp_cloud_server_context import DpCloudServerContext  # noqa: F401
 except Exception:
     pass
+import dargs  # noqa: F401
+
+from dpdispatcher.machine import Machine  # noqa: F401
+from dpdispatcher.submission import Job, Resources, Submission, Task  # noqa: F401
+from dpdispatcher.utils import RetrySignal, retry  # noqa: F401
 
 
 def setUpModule():
