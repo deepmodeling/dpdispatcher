5694e9bab335e0b9249af6c40908f7b1c9812c05
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-04-11 06:49:36 -0400

    noqa: F401
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- tests/lsf/context.py
+++ tests/lsf/context.py
@@ -5,6 +5,12 @@ sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
 )
 
+from dpgen.dispatcher.Dispatcher import Dispatcher  # noqa: F401
+from dpgen.dispatcher.JobStatus import JobStatus  # noqa: F401
+from dpgen.dispatcher.LocalContext import LocalContext, LocalSession  # noqa: F401
+from dpgen.dispatcher.LSF import LSF  # noqa: F401
+from dpgen.dispatcher.SSHContext import SSHContext, SSHSession  # noqa: F401
+
 
 def my_file_cmp(test, f0, f1):
     with open(f0) as fp0:
