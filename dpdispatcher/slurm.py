17acb278af43c42b7ea390da4b51de38ca886d74
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-04-16 18:21:34 -0400

    fix script
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- dpdispatcher/slurm.py
+++ dpdispatcher/slurm.py
@@ -250,7 +250,9 @@ class SlurmJobArray(Slurm):
                 script_command += f"{ii // slurm_job_size})\n"
             script_command += single_script_command
             script_command += self.gen_script_wait(resources=resources)
-            script_command += "\n;;\n"
+            script_command += "\n"
+            if ii % slurm_job_size == slurm_job_size - 1:
+                script_command += ";;\n"
         script_command += "*)\nexit 1\n;;\nesac\n"
         return script_command
 
