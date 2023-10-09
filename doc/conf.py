4a035ee131febeb625943d56d72a27e194fb5b12
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-10-08 20:02:48 -0400

    add sphinx-argparse to dependencies
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- doc/conf.py
+++ doc/conf.py
@@ -38,6 +38,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "numpydoc",
     "sphinx.ext.autosummary",
+    "sphinxarg.ext",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
