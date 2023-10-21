c0518433b2328cb94cdb9b24cb31928ba8944677
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-10-21 05:47:24 -0400

    Revert "wait"
    This reverts commit 5104d164d4a173158803160d71910289c82c0fce.

--- ci/torque/start-torque.sh
+++ ci/torque/start-torque.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 docker-compose up -d --no-build
-while [ `docker exec torque /bin/bash -c ls | wc -l` -ne 1 ]
-do
-    echo "Waiting for nodes to become available";
-    sleep 2
-done
 echo "Torque properly configured"
