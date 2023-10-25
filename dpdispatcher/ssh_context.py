c1868e8cb9cbff4bfd87dd86c00e5edabdac7e74
Yifan Li <yifanl0716@gmail.com>
2023-10-25 17:04:27 -0400

    pass timeout to paramiko's Transport

--- dpdispatcher/ssh_context.py
+++ dpdispatcher/ssh_context.py
@@ -140,6 +140,7 @@ class SSHSession:
         # Make a Paramiko Transport object using the socket
         ts = paramiko.Transport(sock)
         ts.banner_timeout = 60
+        ts.auth_timeout = self.timeout
         ts.use_compression(compress=True)
 
         # Tell Paramiko that the Transport is going to be used as a client
