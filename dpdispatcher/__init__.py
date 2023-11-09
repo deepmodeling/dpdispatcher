c0cd31c5b4da1cd49bfd297ac9277fd02b75d304
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-11-08 23:27:26 -0500

    change __copyright__ and __status__
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- dpdispatcher/__init__.py
+++ dpdispatcher/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "DeepModeling Team"
-__copyright__ = "Copyright 2019"
-__status__ = "Development"
+__copyright__ = "Copyright 2019-2023, DeepModeling"
+__status__ = "Production"
 try:
     from ._version import version as __version__
 except ImportError:
