bd71277e12bbabecf3da7f556e5d132760c9307a
ck <xiaoyeqiannian@163.com>
2023-11-10 14:04:21 -0600

    fix set retry_count=0 is invalid (#408)

--- dpdispatcher/submission.py
+++ dpdispatcher/submission.py
@@ -853,7 +853,7 @@ class Job:
             )
             retry_count = 3
             assert self.machine is not None
-            if hasattr(self.machine, "retry_count") and self.machine.retry_count > 0:
+            if hasattr(self.machine, "retry_count") and self.machine.retry_count >= 0:
                 retry_count = self.machine.retry_count + 1
             if (self.fail_count) > 0 and (self.fail_count % retry_count == 0):
                 last_error_message = self.get_last_error_message()
