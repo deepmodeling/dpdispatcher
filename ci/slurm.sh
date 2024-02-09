f3690e1f8fb938bb02c1dd59de1ca5ee496a4439
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-02-09 15:25:04 -0500

    add CODECOV_TOKEN

--- ci/slurm.sh
+++ ci/slurm.sh
@@ -7,4 +7,4 @@ docker-compose pull
 cd -
 
 docker exec slurmctld /bin/bash -c "cd dpdispatcher && pip install .[test] coverage && coverage run --source=./dpdispatcher -m unittest -v && coverage report"
-docker exec --env-file <(env | grep GITHUB) slurmctld /bin/bash -c "cd dpdispatcher && curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov && ./codecov"
+docker exec --env-file <(env | grep -e GITHUB -e CODECOV) slurmctld /bin/bash -c "cd dpdispatcher && curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov && ./codecov"
