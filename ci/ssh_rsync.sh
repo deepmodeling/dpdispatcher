1b5890ae8b02a93967016e07eca243ab53d5971f
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-04-15 22:33:20 -0400

    CI: setup uv

--- ci/ssh_rsync.sh
+++ ci/ssh_rsync.sh
@@ -10,5 +10,5 @@ cd -
 docker exec server /bin/bash -c "apt-get -y update && apt-get -y install rsync"
 docker exec test /bin/bash -c "apt-get -y update && apt-get -y install rsync"
 
-docker exec test /bin/bash -c "cd /dpdispatcher && pip install .[test] coverage && coverage run --source=./dpdispatcher -m unittest -v && coverage report"
+docker exec test /bin/bash -c "cd /dpdispatcher && pip install uv && uv pip install --system .[test] coverage && coverage run --source=./dpdispatcher -m unittest -v && coverage report"
 docker exec --env-file <(env | grep -e GITHUB -e CODECOV) test /bin/bash -c "cd /dpdispatcher && curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov && ./codecov"
