7df95d6395233d6f23447ab794a7308066492df4
WangSimin123456 <96227963+WangSimin123456@users.noreply.github.com>
2024-05-25 09:51:35 +0800

    Update test_JH_UniScheduler_script_generation.py

--- tests/test_JH_UniScheduler_script_generation.py
+++ tests/test_JH_UniScheduler_script_generation.py
@@ -87,54 +87,6 @@ class TestJHUniSchedulerScriptGeneration(unittest.TestCase):
         )
         self.assertEqual(header_str, benchmark_header)
 
-        custom_flags_str = machine.gen_script_custom_flags_lines(test_job)
-        benchmark_custom_flags = textwrap.dedent(
-            """\
-            #JSUB -M "hpc-gpu-02"
-            #JSUB -W 60:00
-            """
-        )
-        self.assertEqual(custom_flags_str, benchmark_custom_flags)
-
-        env_str = machine.gen_script_env(test_job)
-        benchmark_env = textwrap.dedent(
-            f"""
-            REMOTE_ROOT={task_hash}
-            echo 0 > $REMOTE_ROOT/{job_hash}_flag_if_job_task_fail
-            test $? -ne 0 && exit 1
-
-            module purge
-
-            module load use.own
-            module load deepmd/1.3
-
-            {{ source /data/home/ypliu/scripts/avail_gpu.sh; }} 
-            {{ source /data/home/ypliu/dprun/tf_envs.sh; }} 
-
-            export DP_DISPATCHER_EXPORT=test_foo_bar_baz
-
-            echo 'The summer you were there.'
-            """
-        )
-        self.assertEqual(env_str.split("\n")[2:], benchmark_env.split("\n")[2:])
-
-        footer_str = machine.gen_script_end(test_job)
-        benchmark_footer = textwrap.dedent(
-            f"""\
-
-            cd $REMOTE_ROOT
-            test $? -ne 0 && exit 1
-
-            wait
-            FLAG_IF_JOB_TASK_FAIL=$(cat {job_hash}_flag_if_job_task_fail)
-            if test $FLAG_IF_JOB_TASK_FAIL -eq 0; then touch {job_hash}_job_tag_finished; else exit 1;fi
-
-            echo 'shizuku'
-            echo 'kaori'
-            """
-        )
-        self.assertEqual(footer_str, benchmark_footer)
-
     @unittest.skipIf(sys.platform == "win32", "skip for persimission error")
     def test_template(self):
         with open("jsons/machine_lazy_local_jh_unischeduler.json") as f:
